1c3e2f3b886b5f9c4efed976b9ef090ded44cb71
Trevor Moore <trevor@raindrop.com>
2020-09-01 16:39:20 -0700

    move non test functions

--- common_test.go
+++ common_test.go
@@ -4,25 +4,18 @@ import (
 	"bytes"
 	"crypto"
 	"crypto/aes"
-	"crypto/cipher"
 	"crypto/hmac"
-	"crypto/md5"
-	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"reflect"
-	"strconv"
 	"strings"
-	"sync"
 	"testing"
 	"unicode"
 
-	"github.com/aead/ecdh"
 	"github.com/jeremywohl/flatten"
 	"github.com/pdfcpu/pdfcpu/pkg/api"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
@@ -192,59 +185,6 @@ func TestEncryption(t *testing.T) {
 	fmt.Println(string(decryptFile("sample.txt", "password1")))
 }
 
-const BUFFER_SIZE int = 4096
-const IV_SIZE int = 16
-
-func createHash(key string) string {
-	hasher := md5.New()
-	hasher.Write([]byte(key))
-	return hex.EncodeToString(hasher.Sum(nil))
-}
-
-func encrypt(data []byte, passphrase string) []byte {
-	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-	return ciphertext
-}
-
-func decrypt(data []byte, passphrase string) []byte {
-	key := []byte(createHash(passphrase))
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err.Error())
-	}
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonceSize := gcm.NonceSize()
-	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err.Error())
-	}
-	return plaintext
-}
-
-func encryptFile(filename string, data []byte, passphrase string) {
-	f, _ := os.Create(filename)
-	defer f.Close()
-	f.Write(encrypt(data, passphrase))
-}
-
-func decryptFile(filename string, passphrase string) []byte {
-	data, _ := ioutil.ReadFile(filename)
-	return decrypt(data, passphrase)
-}
-
 func TestStringSplit(t *testing.T) {
 	pathname := "data.owner.name"
 	resultArr := strings.Split(pathname, ".")
@@ -274,56 +214,6 @@ func TestSha3(t *testing.T) {
 	fmt.Printf("%x\n", h)
 }
 
-//genECDH acts as a method on the public key ring, receiving channels as inputs
-func (pkr *publicKeyring) genECDH(pchan chan bool, mchan chan []byte, n int) {
-
-	c25519 := ecdh.X25519()
-
-	privatek, publick, err := c25519.GenerateKey(rand.Reader)
-	if err != nil {
-		fmt.Printf("Failed to generate private/public key pair: %s\n", err)
-	}
-
-	pkr.mux.Lock()
-	pkr.v[n] = publick
-	pkr.mux.Unlock()
-	pkr.wg.Done()
-
-	var k []byte
-
-	chk := <-pchan
-	if chk {
-		k = c25519.ComputeSecret(privatek, pkr.v[(n+1)%2])
-	}
-	buf := []byte("this is a message for authentication. the message is hashed to verify message contents.  the recieved message is hashed again using the same key to make sure that the hash matches")
-	// A MAC with 32 bytes of output has 256-bit security strength -- if you use at least a 32-byte-long key.
-	hash := make([]byte, 64)
-	d := sha3.NewShake256()
-	// Write the key into the hash.
-	d.Write(k)
-	// Now write the data.
-	d.Write(buf)
-	// Read 32 bytes of output from the hash into h.
-	d.Read(hash)
-	mchan <- hash
-
-	block, err := aes.NewCipher(k)
-	if err != nil {
-		panic(err)
-	}
-
-	pkr.mux.Lock()
-	pkr.m[n] = encryptByte(block, []byte("decrypted message from entity"+strconv.Itoa(n)))
-	pkr.mux.Unlock()
-	pchan <- true
-
-	_ = <-mchan
-
-	msg := decryptByte(block, pkr.m[(n+1)%2])
-
-	log.Println("i am entity", strconv.Itoa(n), string(msg))
-}
-
 func TestAsyncECDH(t *testing.T) {
 	pkr := publicKeyring{v: make(map[int]crypto.PublicKey), m: make(map[int][]byte)}
 	//need a waitgroup to wait until both entities exchange their public keys
@@ -351,56 +241,10 @@ func TestAsyncECDH(t *testing.T) {
 	log.Println("encrypted message 1", string(pkr.m[1]))
 }
 
-type ecdhKey struct {
-	PrivateK crypto.PrivateKey
-	PublicK  crypto.PublicKey
-}
-
-// Inc increments the counter for the given key.
-
-type publicKeyring struct {
-	m   map[int][]byte
-	v   map[int]crypto.PublicKey
-	mux sync.Mutex
-	wg  sync.WaitGroup
-}
-
-func TestAES(t *testing.T) {
-	aes.NewCipher([]byte(`asdgsadg`))
-}
-
-//encrypt byte encrypts byte input using cipher block
-func encryptByte(block cipher.Block, value []byte) []byte {
-	// Generate an initialization vector (IV) suitable for encryption.
-	// http://en.wikipedia.org/wiki/Block_cipher_modes_of_operation#Initialization_vector_.28IV.29
-	iv := make([]byte, block.BlockSize())
-	rand.Read(iv)
-	// Encrypt it.
-	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(value, value)
-	// Return iv + ciphertext.
-	return append(iv, value...)
-}
-
-//decryptByte decrypts a byte using cipher block
-func decryptByte(block cipher.Block, value []byte) []byte {
-	if len(value) > block.BlockSize() {
-		// Extract iv.
-		iv := value[:block.BlockSize()]
-		// Extract ciphertext.
-		value = value[block.BlockSize():]
-		// Decrypt it.
-		stream := cipher.NewCTR(block, iv)
-		stream.XORKeyStream(value, value)
-		return value
-	}
-	return nil
-}
-
 func TestEncryptCTR(t *testing.T) {
 	block, err := aes.NewCipher([]byte("1234567890123456"))
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 
 	value := "foobarbaz asdfgasg df adf faffdf dfadsg thsi a a test of streaming encryption using a block cipher"
